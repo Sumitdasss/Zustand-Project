abac65e919cef8265b8286fe1b499b64519d32cd
Sumitdasss <sumitdas46123@gmail.com>
2026-06-06 19:19:24 +0600

    add

--- src/Componant/Layout/Banner.jsx
+++ src/Componant/Layout/Banner.jsx
@@ -1,19 +1,16 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaChevronUp} from "react-icons/fa";
-import Slider from "react-slick";
-const SlickSlider = Slider.default ? Slider.default : Slider;
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
 const Banner = () => {
-    const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-      autoplay: true,
-      prevArrow:false,
-      nextArrow: false,
-  };
+ 
   return (
     <div className="max-w-[1440px] mx-auto   flex flex-col md:flex-row  h-[450px] ">
   
@@ -40,27 +37,78 @@ const Banner = () => {
   <div className="w-full md:w-3/4 mt-10 ml-10">
     
     
-     <SlickSlider  {...settings}>
+     <Swiper
+      modules={[Autoplay, Pagination, Navigation]}
+      spaceBetween={20}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{ clickable: true }}
+     
+    >
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="https://images.pexels.com/photos/50614/pexels-photo-50614.jpeg"
+            alt="Banner 1"
+            className="w-full h-85 object-cover rounded"
+          />
+        </a>
+      </SwiperSlide>
 
- <div>
-        <a href="#"><img src="https://images.pexels.com/photos/50614/pexels-photo-50614.jpeg" alt="Banner 1" className="w-full h-85 object-cover rounded"/></a>
-      </div>
-      <div>
-        <a href="#"><img src="https://images.pexels.com/photos/10054188/pexels-photo-10054188.jpeg" alt="Banner 1" className="w-full h-85 object-cover rounded"/></a>
-      </div>
-      <div>
-        <a href="#"><img src="https://images.pexels.com/photos/12920900/pexels-photo-12920900.jpeg" alt="Banner 1" className="w-full h-85 object-cover rounded"/></a>
-      </div>
-      <div>
-        <a href="#"><img src="https://images.pexels.com/photos/30726378/pexels-photo-30726378.jpeg" alt="Banner 1" className="w-full h-85 object-cover rounded"/></a>
-      </div>
-      <div>
-        <a href="#"><img src="https://images.pexels.com/photos/6804613/pexels-photo-6804613.jpeg" alt="Banner 1" className="w-full h-85 object-cover rounded"/></a>
-      </div>
-      <div>
-        <a href="#"><img src="https://images.pexels.com/photos/15592023/pexels-photo-15592023.jpeg" alt="Banner 1" className="w-full h-85 object-cover rounded"/></a>
-      </div>
-     </SlickSlider>
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="https://images.pexels.com/photos/10054188/pexels-photo-10054188.jpeg"
+            alt="Banner 2"
+            className="w-full h-85 object-cover rounded"
+          />
+        </a>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="https://images.pexels.com/photos/12920900/pexels-photo-12920900.jpeg"
+            alt="Banner 3"
+            className="w-full h-85 object-cover rounded"
+          />
+        </a>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="https://images.pexels.com/photos/30726378/pexels-photo-30726378.jpeg"
+            alt="Banner 4"
+            className="w-full h-85 object-cover rounded"
+          />
+        </a>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="https://images.pexels.com/photos/6804613/pexels-photo-6804613.jpeg"
+            alt="Banner 5"
+            className="w-full h-85 object-cover rounded"
+          />
+        </a>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <a href="#">
+          <img
+            src="https://images.pexels.com/photos/15592023/pexels-photo-15592023.jpeg"
+            alt="Banner 6"
+            className="w-full h-85 object-cover rounded"
+          />
+        </a>
+      </SwiperSlide>
+    </Swiper>
   </div>
 
 </div>
